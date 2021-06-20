cc54751ababb03bd9bbaf3f17be452ba5452ca40
Melisa Beysümengü <61505693+melisaBeysumengu@users.noreply.github.com>
2021-06-21 01:24:57 +0300

    initial

---
+++ pages/about.js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+
+const About = () => {
+    return (
+        <>
+            <Head>
+            <title>Ninja List | About</title>
+            <meta name="keywords" constent="ninjas" />
+            </Head>
+            <div>
+                <h1>About</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
+            </div>
+        </>
+    );
+}
+
+export default About;
---
+++ pages/ninjas/test.js
@@ -0,0 +1,11 @@
+const Test = () => {
+    return ( 
+        <div>
+            <h1>Test</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
+        </div>
+    );
+}
+
+export default Test;
