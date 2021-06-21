9c0757ab93b9871e4d884f004f6a63af1e821436
Melisa Beysümengü <61505693+melisaBeysumengu@users.noreply.github.com>
2021-06-21 20:55:31 +0300

    initial

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,5 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import Navbar from '../comps/navbar'
-import Footer from '../comps/Footer'
 import Link from 'next/link'
 export default function Home() {
   return (
